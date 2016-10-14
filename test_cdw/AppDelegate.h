17f2dd1e0f7b7d587dea3e858c8aa831e175e1d7
杨光 <you@example.com>
2016-10-14 15:50:33 +0800

    首次更改

---
+++ test_cdw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_cdw
+//
+//  Created by 杨光 on 2016/10/14.
+//  Copyright © 2016年 杨光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_cdw/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test_cdw
+//
+//  Created by 杨光 on 2016/10/14.
+//  Copyright © 2016年 杨光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
